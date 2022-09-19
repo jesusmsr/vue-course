cc33e26b35d53b7e27dbd3331d42f4d314a6e89b
sanchezj <jesus.sanchez@creamteam.de>
2022-09-19 10:51:17 +0200

    methods

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course and learn Vue',
             vueLink: 'https://vuejs.org/'
         }
+    },
+    methods: {
+        outputGoal() {
+            const num = Math.random();
+            if (num > 0.5) {
+                return 'Learn Vue';
+            } else {
+                return 'Master Vue';
+            }
+        }
     }
 });
 
