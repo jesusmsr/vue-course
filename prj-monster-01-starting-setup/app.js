741f8de24a386fe876bd167d9ae7a4a47df0c0ed
sanchezj <jesus.sanchez@creamteam.de>
2022-09-28 16:20:16 +0200

    special attack logic

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -4,6 +4,7 @@ const app = Vue.createApp({
             monsterHp: 100,
             playerHp: 100,
             combatLog: [],
+            specialAttackCounter: 0,
         }
     },
     computed: {
@@ -16,10 +17,18 @@ const app = Vue.createApp({
             return {
                 width: this.playerHp + '%'
             }
+        },
+        specialAttackAvailable() {
+            if (this.specialAttackCounter > 0) {
+                return true;
+            } else if (this.specialAttackCounter == 0) {
+                return false;
+            }
         }
     },
     methods: {
         attackMonster() {
+            this.specialAttackCounter -= 1;
             const attackValue = Math.floor(Math.random() * (12 - 6)) + 5;
             this.monsterHp -= attackValue;
             this.combatLog.push('Atacao');
@@ -28,6 +37,12 @@ const app = Vue.createApp({
         attackPlayer() {
             const attackValue = Math.floor(Math.random() * (15 - 8)) + 5;
             this.playerHp -= attackValue
+        },
+        specialAttackMonster() {
+            this.specialAttackCounter = 3;
+            const attackValue = Math.floor(Math.random() * (30 - 12)) + 5;
+            this.monsterHp -= attackValue;
+            this.attackPlayer();
         }
     }
 });
